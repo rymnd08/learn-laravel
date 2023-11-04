b7b2446e83bf72c90b55dd4834ca2a91338e771c
rymnd0800 <raymundregalado8@gmail.com>
2023-11-04 19:15:43 +0800

    adjust card screen sizes

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    <div class="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 ">
+    <div class="grid grid-cols-1 gap-10 lg:grid-cols-2 xl:grid-cols-3 ">
 
         <!-- CARD -->
         <div class="h-48 w-full border-rose-600 border-2 rounded-md hover:shadow-md hover:shadow-rose-400 m-auto sm:w-full">
