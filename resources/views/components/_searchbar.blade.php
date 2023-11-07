c360524fca0b8438ab89d35510220d955d49eac5
rymnd0800 <raymundregalado8@gmail.com>
2023-11-07 23:34:31 +0800

    add search component

---
+++ resources/views/components/_searchbar.blade.php
@@ -0,0 +1,6 @@
+<!-- Search bar -->
+<div class="max-w-7xl mx-auto border mt-5 mb-3 p-2 flex items-center gap-2">
+    <i class="bi bi-search"></i>
+    <input type="text" placeholder="Search" class="w-full focus:outline-none ">
+    <button  class="bg-laravel text-white py-1 px-3 rounded-full font-semibold hover:scale-110">Search</button>
+</div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,9 +1,11 @@
 <x-HTML>
   <x-_navbar />
 
-    <div class="max-w-7xl mx-auto mt-5">
+<x-_searchbar />
+
+    <div class="max-w-7xl mx-auto ">
         <div class="flex justify-end mb-2">
-          <button class="py-1 px-3 bg-laravel text-white font-bold rounded-full add-button">Add <i class="bi bi-plus-circle"></i></button>
+          <button class="py-1 px-3 bg-laravel text-white font-semibold rounded-full add-button hover:scale-110 mr-2 xl:mr-0">Add <i class="bi bi-plus-circle"></i></button>
         </div>
         <!-- VIEW OUTPUT -->
         @yield('content')
